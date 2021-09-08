072937a9740960fb8a88b16ca139057d39275a41
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-09-07 23:06:29 -0400

    added Workout model

--- models/Workout.js
+++ models/Workout.js
@@ -2,36 +2,41 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+//As a user, I want to be able to view create and track daily workouts. I want to be able to log multiple exercises in a workout on a given day.
+//I should also be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, I should be able to track my distance traveled.
+
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now,
   },
-  exercise: [],
-
-  //   username: {
-  //     type: String,
-  //     trim: true,
-  //     required: "Username is Required",
-  //   },
-
-  //   password: {
-  //     type: String,
-  //     trim: true,
-  //     required: "Password is Required",
-  //     validate: [({ length }) => length >= 6, "Password should be longer."],
-  //   },
-
-  //   email: {
-  //     type: String,
-  //     unique: true,
-  //     match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-  //   },
-
-  //   isCool: {
-  //     type: Boolean,
-  //     default: false,
-  //   },
+  exercise: [
+    {
+      name: {
+        type: String,
+        trim: true,
+      },
+      type: {
+        type: String,
+        trim: true,
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 // WorkoutSchema.methods.coolifier = function () {
