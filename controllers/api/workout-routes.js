7652996f7ea0fd7cf0301654490c7c0c0f0b330c
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-09-08 20:56:16 -0400

    reworked routes got get post and put mostly working

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -3,16 +3,11 @@ const db = require("../../models");
 
 // GET /api/workouts
 router.get("/", (req, res) => {
-  // db.Workout.find({}, (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     res.json(data);
-  //   }
-  // });
   db.Workout.find({})
     .then((dbWorkout) => {
+      console.log("start getLastWorkout: GET /api/workouts -------");
       console.log(dbWorkout);
+      console.log("end getLastWorkout: GET /api/workouts -------");
       res.json(dbWorkout);
     })
     .catch((err) => {
@@ -21,24 +16,54 @@ router.get("/", (req, res) => {
   //res.send("hit GET /api/workouts ok"); //this was just for setup
 });
 
-// GET /api/workouts/:id
-router.get("/:id", (req, res) => {
-  res.send("hit GET /api/workouts/:id ok");
-});
-
-// POST /api/workouts/:id
-router.post("/:id", (req, res) => {
-  res.send("hit POST /api/workouts/:id ok");
+// POST /api/workouts
+router.post("/", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      console.log("start createWorkout: POST /api/workouts -------");
+      console.log(body);
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+      console.log("end createWorkout: POST /api/workouts -------");
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+  //res.send("hit POST /api/workouts ok");  //this was just for setup
 });
 
 // PUT /api/workouts/:id
 router.put("/:id", (req, res) => {
-  res.send("hit PUT /api/workouts/:id ok");
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      console.log("start addExercise: PUT /api/workouts/:id -------");
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+      console.log("end addExercise: PUT /api/workouts/:id -------");
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+  //res.send("hit PUT /api/workouts/:id ok");  //this was just for setup
 });
 
 // /api/workouts/range
 router.get("/range", (req, res) => {
-  res.send("hit GET /api/workouts/range ok");
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      console.log("start getWorkoutsInRange: GET /api/workouts/range -------");
+      console.log(dbWorkout);
+      console.log("end getWorkoutsInRange: GET /api/workouts/range -------");
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+  //res.send("hit GET /api/workouts/range ok"); //this was just for setup
 });
 
 module.exports = router;
--- models/Workout.js
+++ models/Workout.js
@@ -10,7 +10,7 @@ const WorkoutSchema = new Schema({
     type: Date,
     default: Date.now,
   },
-  exercise: [
+  exercises: [
     {
       name: {
         type: String,
